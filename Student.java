39d230fffdc647bdaef3d8b3756604bd20c515f1
manyakn4-svg <manyakn4@gmail.com>
2026-09-23 23:20:01 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,8 @@
+ class Student{
+    public static void main(String[] args) 
+    {
+        System.out.println("name=manya");
+        System.out.println("college=cit");
+        System.out.println("aim=to become cloud engineer");
+    }   
+}
